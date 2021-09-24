dd0aabab2164c5b9096efe038e6282a4faf2b1cb
momojuro <momojurosan@gmail.com>
2021-09-24 03:53:23 +0200

    Update fsociety tribute's sunfish setup for the next 2.00 update (3/3)
    - BBR v2 is replaced by BBR in the kernel source code
    - page-cluster is set to 0 after the execution of swapon
    - AnyKernel3 upstream sync (https://github.com/fsociety-tribute/AnyKernel3/commit/84caeb46e51e85f405a4fdc839254339a670391d#diff-703f70c41865bc8e07b7f5da93d9d052658d938a58435ee3f04f5df3a4e65794 - https://github.com/fsociety-tribute/AnyKernel3/commit/f88f90e3d2f48924256e9f6cc66819a14afed2bf#diff-703f70c41865bc8e07b7f5da93d9d052658d938a58435ee3f04f5df3a4e65794)

--- ramdisk/overlay.d/sbin/init.fsociety_tribute_boot.sh
+++ ramdisk/overlay.d/sbin/init.fsociety_tribute_boot.sh
@@ -10,9 +10,6 @@ sleep 30;
 # Disable zRAM
 swapoff /dev/block/zram0;
 
-# Set BBRv2 as the default TCP cong. algo.
-echo "bbr2" > /proc/sys/net/ipv4/tcp_congestion_control; # stock is westwood
-
 # Tune schedutil's down rate limit values for better battery life
 echo 25000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us; # stock is 20000
 echo 25000 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_us; # stock is 20000
@@ -21,8 +18,8 @@ echo 25000 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_u
 echo 1 > /sys/block/zram0/reset;
 echo "lzo-rle" > /sys/block/zram0/comp_algorithm;
 echo 2906652672 > /sys/block/zram0/disksize;
-echo 0 > /proc/sys/vm/page-cluster;
 mkswap /dev/block/zram0;
 swapon /dev/block/zram0 -p 32758;
+echo 0 > /proc/sys/vm/page-cluster;
 
 echo "fsociety tribute's tuning: completed." >> /dev/kmsg

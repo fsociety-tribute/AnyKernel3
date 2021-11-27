7acf3fd4fcb9a05d05be05e0301968f18ac1ee18
momojuro <momojurosan@gmail.com>
2021-11-27 08:28:05 +0100

    Update fsociety tribute's sunfish setup for the next 2.00 update
    - Set bbr as the default TCP cong. algo.
    - Update the comments and copyright section

--- ramdisk/overlay.d/sbin/init.fsociety_tribute_boot.sh
+++ ramdisk/overlay.d/sbin/init.fsociety_tribute_boot.sh
@@ -1,30 +1,32 @@
 #!/system/bin/sh
 
-# (c) 2019-2020 changes for blu_spark by eng.stk
+# (c) 2019-2021 changes for blu_spark by eng.stk
 # (c) 2020-2021 PK's Tuning Script for Pixel 4/XL by pkgnex
 # (c) 2021 fsociety tribute by momojuro
 
 # Wait to set the init values
 sleep 30;
 
-# Disable zRAM
-swapoff /dev/block/zram0;
+# Set bbr as the default TCP cong. algo.
+echo "bbr" > /proc/sys/net/ipv4/tcp_congestion_control; # stock is cubic
 
-# Set zen scheduler for the sda block
+# Tune the sda block's I/O parameters
 echo "zen" > /sys/block/sda/queue/scheduler;
 echo 160 > /sys/block/sda/queue/nr_requests;
-echo 640 > /sys/block/sda/queue/read_ahead_kb;
+echo 640 > /sys/block/sda/queue/read_ahead_kb; # 640 = 160 nr_requests custom value x 4
 
 # Tune schedutil's down rate limit values for better battery life
 echo 25000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us; # stock is 20000
 echo 25000 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_us; # stock is 20000
 
 # Set zRAM config
+swapoff /dev/block/zram0;
+sleep 10;
 echo 1 > /sys/block/zram0/reset;
 echo "lzo-rle" > /sys/block/zram0/comp_algorithm;
 echo 2906652672 > /sys/block/zram0/disksize;
 mkswap /dev/block/zram0;
 swapon /dev/block/zram0 -p 32758;
-echo 0 > /proc/sys/vm/page-cluster;
+echo 0 > /proc/sys/vm/page-cluster; # swap pages one at a time
 
 echo "fsociety tribute's tuning: completed." >> /dev/kmsg

6d82017a6124fc108e749e1c6416e3299d2b402d
momojuro <momojurosan@gmail.com>
2022-01-01 23:18:21 +0100

    Update fsociety tribute's tuning script
    - Set cfq back as the default I/O scheduler for the sda block device
    - Set zRAM's compression algorithm back to lz4*
    - Calculate zRAM's disksize using half of the total RAM's value**
    - Revert page-cluster to its default value
    
    * https://indico.fnal.gov/event/16264/contributions/36466/attachments/22610/28037/Zstd__LZ4.pdf
    
    ** Originally taken from: https://github.com/markVnl/zram/blob/el8/SOURCES/zramstart

--- ramdisk/overlay.d/sbin/init.fsociety_tribute_boot.sh
+++ ramdisk/overlay.d/sbin/init.fsociety_tribute_boot.sh
@@ -10,9 +10,6 @@ sleep 30;
 # Set bbr as the default TCP cong. algo.
 echo "bbr" > /proc/sys/net/ipv4/tcp_congestion_control; # stock is cubic
 
-# Set fiops as the default I/O scheduler for the sda block device (HLOS LUN)
-echo "fiops" > /sys/block/sda/queue/scheduler;
-
 # Tune schedutil's down rate limit values for better battery life
 echo 25000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us; # stock is 20000
 echo 25000 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_us; # stock is 20000
@@ -20,10 +17,9 @@ echo 25000 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_u
 # Set zRAM config
 swapoff /dev/block/zram0;
 echo 1 > /sys/block/zram0/reset;
-echo "lzo-rle" > /sys/block/zram0/comp_algorithm;
-echo 2906652672 > /sys/block/zram0/disksize;
+echo "lz4" > /sys/block/zram0/comp_algorithm;
+echo 2933049344 > /sys/block/zram0/disksize;
 mkswap /dev/block/zram0;
 swapon /dev/block/zram0 -p 32758;
-echo 0 > /proc/sys/vm/page-cluster; # swap pages one at a time
 
 echo "fsociety tribute's tuning: completed." >> /dev/kmsg

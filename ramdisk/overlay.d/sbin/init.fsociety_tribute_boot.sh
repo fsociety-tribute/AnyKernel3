e0761f55f0c4db79a3e90082fba47a4504ee3177
momojuro <momojurosan@gmail.com>
2022-02-05 16:21:59 +0100

    Update fsociety tribute's tuning script
    - The custom schedutil tunables aren't required anymore as the governor has got the recent code updates

--- ramdisk/overlay.d/sbin/init.fsociety_tribute_boot.sh
+++ ramdisk/overlay.d/sbin/init.fsociety_tribute_boot.sh
@@ -1,7 +1,6 @@
 #!/system/bin/sh
 
 # (c) 2019-2021 changes for blu_spark by eng.stk
-# (c) 2020-2021 PK's Tuning Script for Pixel 4/XL by pkgnex
 # (c) 2021 fsociety tribute by momojuro
 
 # Post-boot complete script's execution delay
@@ -9,10 +8,6 @@ sleep 30;
 
 # Set bbr as the default TCP cong. algo.
 echo "bbr" > /proc/sys/net/ipv4/tcp_congestion_control; # stock is cubic
-
-# Tune schedutil's down rate limit values for better battery life
-echo 25000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us; # stock is 20000
-echo 25000 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_us; # stock is 20000
 
 # Set zRAM config
 swapoff /dev/block/zram0;
